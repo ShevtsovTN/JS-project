14f22b4925591a2d521a375c4b2d98072dd2d741
ShevtsovTN <tim_mega@bk.ru>
2020-10-11 20:55:10 +0200

    environment on

--- src/core/dom.js
+++ src/core/dom.js
@@ -1,8 +1,42 @@
 class Dom {
+  constructor(selector) {
+    this.$el = typeof selector === 'string'
+      ? document.querySelector(selector)
+      : selector
+  }
+
+  html(html) {
+    if (typeof html === 'string') {
+      this.$el.innerHTML = html
+      return this
+    }
+    return this.$el.outerHTML.trim()
+  }
+
+  on() {}
+
+  clear() {
+    this.html('')
+    return this
+  }
+
+  append(node) {
+    if (node instanceof Dom) {
+      node = node.$el
+    }
+    if (Element.prototype.append) {
+      this.$el.append(node)
+    } else {
+      this.$el.appendChild(node)
+    }
+    return this
+  }
 }
 
-export function $() {
-  return new Dom()
+$('div').html('<h1>Test</h1>').clear()
+
+export function $(selector) {
+  return new Dom(selector)
 }
 
 $.create = (tagName, classes = '') => {
@@ -10,5 +44,5 @@ $.create = (tagName, classes = '') => {
   if (classes) {
     el.classList.add(classes)
   }
-  return el
+  return $(el)
 }
